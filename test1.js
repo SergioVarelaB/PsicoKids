2c20580d33c2e51ae7d47fd5895d4f605255a6db
JCPedrozaM <JCPedrozaM@users.noreply.github.com>
2020-06-03 22:38:52 -0600

    test1 js completo ahora si

--- test1.js
+++ test1.js
@@ -6,16 +6,22 @@ function respuesta(a, b) {
 
 function resultado() {
     var resultado = 0;
+    var cont = 0;
     respuestas.forEach(element => {
+        cont++;
         resultado += element;
     });
-    if (resultado < 15) {
-        document.getElementById("resultado").innerHTML = "Autoestima muy baja. La persona cree poco en sus posibilidades y que necesita estímulos para confiar más en sí mismo.";
-    } else if (resultado > 15 && resultado <= 25) {
-        document.getElementById("resultado").innerHTML = "Autoestima saludable. Cierto que se puede mejorar, pero cree en él mismo y en sus posibilidades. Sabe que puede mejorar pero que no está por debajo de nadie. ";
-    } else if (resultado >= 27) {
-        document.getElementById("resultado").innerHTML = "Autoestima muy fuerte.";
+    if (cont == 10) {
+        if (resultado < 15) {
+            document.getElementById("resultado").innerHTML = "Autoestima muy baja. La persona cree poco en sus posibilidades y que necesita estímulos para confiar más en sí mismo.";
+        } else if (resultado > 15 && resultado <= 25) {
+            document.getElementById("resultado").innerHTML = "Autoestima saludable. Cierto que se puede mejorar, pero cree en él mismo y en sus posibilidades. Sabe que puede mejorar pero que no está por debajo de nadie. ";
+        } else if (resultado >= 27) {
+            document.getElementById("resultado").innerHTML = "Autoestima muy fuerte.";
+        } else {
+            document.getElementById("resultado").innerHTML = "Exceso de autoestima. Puede ser peligrosa, ya que puede denotar problemas para analizar la realidad o el caso de niños demasiado confiados y complacientes consigo mismos";
+        }
     } else {
-        document.getElementById("resultado").innerHTML = "Exceso de autoestima. Puede ser peligrosa, ya que puede denotar problemas para analizar la realidad o el caso de niños demasiado confiados y complacientes consigo mismos";
+        document.getElementById("resultado").innerHTML = "El test no está terminado. Regresa y contesta todas las preguntas";
     }
 }
